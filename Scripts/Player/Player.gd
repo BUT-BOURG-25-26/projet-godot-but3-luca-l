658c13120e07bce61c4ad3820d3a3f89f896001c
LINO LUCA p2309118 <luca.lino@etu.univ-lyon1.fr>
2025-11-17 11:37:39 +0100

    Movement du joueur via le clavier

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,1 +1,33 @@
-extends Node
+class_name Player
+extends CharacterBody3D
+
+# Gameplay
+var move_inputs: Vector2
+var currentLevel = 1
+var dammage = 1
+
+# Health
+@export var maxPv: int = 50
+@export var currentPv: int = maxPv
+
+# Movement 
+@export var moveSpeed: float = 5.0
+
+func _physics_process(delta: float) -> void:
+	read_move_inputs()
+	move_inputs *= moveSpeed * delta
+
+	if move_inputs != Vector2.ZERO:
+		global_position += Vector3(move_inputs.x, 0.0, move_inputs.y)
+		var look_direction = Vector3(move_inputs.x, 0, move_inputs.y).normalized()
+		look_at(global_position - look_direction, Vector3.UP)
+		
+	rotation_degrees.x = 0
+	rotation_degrees.z = 0
+
+
+func read_move_inputs():
+	move_inputs.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	move_inputs.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	move_inputs = move_inputs.normalized()
+	return

21460f20de9ebe949ee1d9d0b0393cb5c6bd9c88
LINO LUCA p2309118 <luca.lino@etu.univ-lyon1.fr>
2025-11-17 17:43:35 +0100

    Melee enemy attack, pas encore finit

--- Scripts/Enemy/Enemy.gd
+++ Scripts/Enemy/Enemy.gd
@@ -1,2 +1,25 @@
 class_name Enemy
 extends CharacterBody3D
+
+# Gameplay
+var player: Player
+var currentLevel = 1
+@export var dammage = 1
+
+# Health
+@export var maxPv: int = 10
+@export var currentPv: int = maxPv
+
+# Movement 
+@export var moveSpeed: float = 3.0
+
+func TakeDammage(dammage: int) -> void:
+	if currentPv - dammage > 0:
+		currentPv -= dammage
+	else:
+		queue_free()
+	print("Enemy : " + str(currentPv))
+	return
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("Player") as Player

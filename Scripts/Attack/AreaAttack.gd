f9866ec0522bfb6e2698024bd0df3910ca895cf3
p2309118 <luca.lino@etu.univ-lyon1.fr>
2025-12-09 22:20:05 +0100

    Attaque en zone (toute les 1.5s)

--- Scripts/Attack/AreaAttack.gd
+++ Scripts/Attack/AreaAttack.gd
@@ -1,6 +1,16 @@
 class_name AreaAttack
 extends Area3D
 
-@export var damageTiming: float = 0.5
+@export var damageTiming: float = 1.5
 @onready var attackTimer:Timer = $Timers/AttackTimer
 @export var damage = 1
+
+func _ready() -> void:
+	attackTimer.start(damageTiming)
+	attackTimer.timeout.connect(DetectCollision)
+
+func DetectCollision() -> void:
+	var overlapping_bodies = get_overlapping_bodies()
+	for body in overlapping_bodies:
+		if body is Enemy:
+			body.TakeDammage(damage)
